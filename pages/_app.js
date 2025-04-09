293879a40e1fa4738eb511f7ac2ef61ab7fa4879
Sonar-glitch <furqanz.email@gmail.com>
2025-04-09 18:34:43 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 // Create this file at pages/_app.js
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
