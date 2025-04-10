9ce767c29293c39e02a4b5399954cf1bda49b762
Sonar-glitch <furqanz.email@gmail.com>
2025-04-09 23:22:07 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
-  // Disable static exports completely
-  output: 'standalone',
-  
-  // Customize the build process
-  webpack: (config, { isServer }) => {
-    // Add any webpack customizations here
-    return config;
-  },
-  
-  // Disable automatic static optimization for authenticated pages
-  unstable_runtimeJS: true,
-  
-  // Specify which pages should not be statically optimized
-  unstable_excludeDefaultMomentLocales: true
-};
+  // Remove these deprecated options:
+  // unstable_runtimeJS: false,
+  // unstable_excludeDefaultMomentLocales: true,
+}
 
 module.exports = nextConfig;
