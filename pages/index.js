f0121d5cc14a27c64db521b727d70e1fe861319c
Sonar-glitch <furqanz.email@gmail.com>
2025-04-18 17:19:49 -0400

    Apply final fixes to TIKO design

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Anton&family=Archivo+Black&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Anton&family=Archivo+Black&family=Inter:wght@400;500;600;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
         
         {/* Open Graph tags for social sharing */}
         <meta property="og:title" content="TIKO | Find your next night out" />
