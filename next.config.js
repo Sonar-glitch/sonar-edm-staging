c276bcf9b69705466efa1c22b4ea352edee33958
Sonar-glitch <furqanz.email@gmail.com>
2025-04-20 01:19:57 -0400

    Fix: alias config + backtick fetch syntax

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-});
+const path = require('path');
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  images: {
-    domains: ['i.scdn.co', 'mosaic.scdn.co', 'platform-lookaside.fbsbx.com'],
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
   experimental: {
-    optimizeCss: true
-  }
-});
+    optimizeCss: true,
+  },
+};
