de9380f1325cb6c3e0c68180a077b408bc798176
Sonar-glitch <furqanz.email@gmail.com>
2025-08-21 20:18:09 -0400

    chore: create minimal clean branch clean-main

--- check_db_config.js
+++
@@ -1,3 +0,0 @@
-console.log('MONGODB_URI:', process.env.MONGODB_URI ? '[CONFIGURED]' : '[NOT SET]');
-console.log('MONGODB_DB:', process.env.MONGODB_DB || 'test');
-console.log('Full URI (partial):', process.env.MONGODB_URI ? process.env.MONGODB_URI.split('?')[0] : 'not set');
